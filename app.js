a79b7c9e582b446abb00ce18924013daad2e565a
juniorfrey <48813208+juniorfrey@users.noreply.github.com>
2022-10-14 22:39:43 +0000

    Configuración de librerias

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
 
+const express = require('express');
+
+const app = express();
+
+// Configuración del encoded y formato JSON
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
+
+//ruta de ejemplo
+app.get('/', (pet, res) => {
+    res.send('<h1> PROYECTO EN BLANCO </h1>')
+});
+
+// servidor corriendo en puerto 3000
+app.listen(5000, (pet, res) => {
+    console.log('SERVIDOR CORRIENDO');
+});
